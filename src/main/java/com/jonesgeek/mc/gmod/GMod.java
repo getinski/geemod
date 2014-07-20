a9833a6c1b42999a4c4f641c3f6e845dc3bce6f4
getinski <ben@jonesgeeks.com>
2014-07-19 20:38:53 -0600

    Created GeeWillickerSword and MetalDetector

--- src/main/java/com/jonesgeek/mc/gmod/GMod.java
+++ src/main/java/com/jonesgeek/mc/gmod/GMod.java
@@ -1,17 +1,15 @@
 package com.jonesgeek.mc.gmod;
 
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+import com.jonesgeek.mc.gmod.item.GeeWillickerSword;
 import com.jonesgeek.mc.gmod.item.GrapplingHook;
+import com.jonesgeek.mc.gmod.item.MetalDetector;
 import com.jonesgeek.mc.gmod.tabs.GTab;
 
-import net.minecraft.block.Block;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -20,7 +18,9 @@ public class GMod {
     public static final String MODID = "gmod";
     public static final String VERSION = "1.0";
 
+    public static Item metaldetect;
     public static Item grapplingHook;
+    public static Item gsword;
     public static CreativeTabs gTab;
     
     @EventHandler
@@ -29,8 +29,12 @@ public class GMod {
     	gTab = new GTab();
     	
     	grapplingHook = new GrapplingHook(gTab);
+    	gsword = new GeeWillickerSword(gTab);
+    	metaldetect = new MetalDetector(gTab);
     	
     	GameRegistry.registerItem(grapplingHook, "grapplingHook");
+    	GameRegistry.registerItem(gsword, "gsword");
+    	GameRegistry.registerItem(metaldetect,"metaldetect");
     	
     	/*GameRegistry.addRecipe(new ItemStack(Items.cookie, 64), new Object[]{
     		"AAA",
