a69bac665a7c9684dbeb47f5a57acf22aaa15e1c
jonesgeek <will@jonesgeeks.com>
2014-08-05 19:06:09 -0600

    OreDetector now has use

--- src/main/java/com/jonesgeek/mc/gmod/item/OreDetector.java
+++ src/main/java/com/jonesgeek/mc/gmod/item/OreDetector.java
@@ -8,6 +8,7 @@ import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
 import org.lwjgl.input.Mouse;
@@ -49,10 +50,66 @@ public class OreDetector extends Item {
 			Entity entity, int i1, boolean held) {
 		super.onUpdate(itemStack, world, entity, i1, held);
 		if(held && Mouse.isButtonDown(1) && blockToDetect != null) {
-			world.playSoundAtEntity(entity, "random.bow", 1.0f, 1.0f);
+			Location location = new Location(entity);
+			
+			int depth = isBlockBeneath(world, location, sensitivity * 10, Block.getIdFromBlock(blockToDetect));
+			if( depth > -1) {
+				System.out.println("Block that is in range: " + depth);
+				world.playSoundAtEntity(entity, "random.bow", 1.0f, 1.0f);
+			}
 		}
 	}
 	
+	/**
+	 * 
+	 * @param world
+	 * @param x
+	 * @param y
+	 * @param z
+	 * @param depth
+	 * @param searchForId
+	 * @return
+	 */
+	private int isBlockBeneath(World world, Location loc, int depth, int searchForId) {
+		int yDepth = loc.getY() - depth;
+		for( int ySearch = loc.getY(); ySearch >= yDepth; ySearch--) {
+			Block block = world.getBlock(loc.getX(), ySearch, loc.getZ());
+        	int blockId = Block.getIdFromBlock(block);
+            if (blockId == searchForId) {
+                return Math.abs(loc.getY() - ySearch);
+             }
+		}
+		return -1;
+	}
 	
+	private class Location {
+		private int x, y, z;
+		private Location(Entity entity) {
+			x = MathHelper.floor_double(entity.posX);
+			y = MathHelper.floor_double(entity.posY - 0.20000000298023224D);
+			z = MathHelper.floor_double(entity.posZ);
+		}
+		
+		/**
+		 * @return the x
+		 */
+		public int getX() {
+			return x;
+		}
+		
+		/**
+		 * @return the y
+		 */
+		public int getY() {
+			return y;
+		}
+		
+		/**
+		 * @return the z
+		 */
+		public int getZ() {
+			return z;
+		}
+	}
 	
 }
